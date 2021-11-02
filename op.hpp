df47f400fc0fed6b045d441cfffb8d7b31888437
rfairooz <rfair008@ucr.edu>
2021-11-01 17:43:23 -0700

    finalized op.hpp and tests for it

--- op.hpp
+++ op.hpp
@@ -2,12 +2,22 @@
 #define __OP_HPP__
 
 #include "base.hpp"
+#include <iostream>
+#include <string>
+#include <iomanip>
+#include <sstream>
 
 class Op : public Base {
+    private:
+        double var = 0;
     public:
-        Op(double value) : Base() { }
-        virtual double evaluate() { return 0.0; }
-        virtual std::string stringify() { return ""; }
+        Op(double value) : Base() {
+            var = value;
+        }
+        double evaluate() { return var; }
+        std::string stringify(){            
+             return std::to_string(var);
+        }
 };
 
 #endif //__OP_HPP__
--- op_test.hpp
+++ op_test.hpp
@@ -10,4 +10,51 @@ TEST(OpTest, OpEvaluateNonZero) {
     EXPECT_EQ(test->evaluate(), 8);
 }
 
+TEST(OpTest, OpEvaluateZero){
+    Op* test = new Op(0);
+    EXPECT_EQ(test->evaluate(), 0);
+}
+
+TEST(OpTest, OpEvaluateNegative){
+    Op* test = new Op(-1);
+    EXPECT_EQ(test->evaluate(), -1);
+}
+
+TEST(OpTest, OpEvaluateDecimal){
+    Op* test = new Op(1.334);
+    EXPECT_EQ(test->evaluate(), 1.334);
+}
+
+TEST(OpTest, OpEvaluateNegativeDecimal){
+    Op* test = new Op(-2.11899);
+    EXPECT_EQ(test->evaluate(), -2.11899);
+}
+
+TEST(OpTest, OpStringifyZero){
+    Op* test = new Op(0);
+    EXPECT_EQ(test->stringify(), "0.000000");    
+}
+
+TEST(OpTest, OpStringifyNonZero){
+    Op* test = new Op(9);
+    EXPECT_EQ(test->stringify(), "9.000000");
+}
+
+TEST(OpTest, OpStringifyDecimal) {
+    Op* test = new Op(7.1123);
+    EXPECT_EQ(test->stringify(), "7.112300");
+}
+
+TEST(OpTest, OpStringifyNegative){
+    Op* test = new Op(-3);
+    EXPECT_EQ(test->stringify(), "-3.000000");
+}
+
+TEST(OpTest, OpStringifyNegativeDec){
+    Op* test = new Op(-3.444419);
+    EXPECT_EQ(test->stringify(), "-3.444419");
+}
+
+
+
 #endif //__OP_TEST_HPP__
