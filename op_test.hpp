9d8dc3cfaa5dbcaea8b76b2314d71cc383430699
rfairooz <rfair008@ucr.edu>
2021-11-03 16:53:07 -0700

    finished add class and added tests

--- op_test.hpp
+++ op_test.hpp
@@ -2,8 +2,8 @@
 #define __OP_TEST_HPP__
 
 #include "gtest/gtest.h"
-
 #include "op.hpp"
+#include "add.hpp"
 
 TEST(OpTest, OpEvaluateNonZero) {
     Op* test = new Op(8);
@@ -55,6 +55,68 @@ TEST(OpTest, OpStringifyNegativeDec){
     EXPECT_EQ(test->stringify(), "-3.444419");
 }
 
+TEST(AddTest, AddInt){
+     Base* three = new Op(3);
+     Base* four = new Op(4);
+     Base* test = new Add(three, four);
+     EXPECT_EQ(test->evaluate(), 7);
+     EXPECT_EQ(test->stringify(), "(3.000000 + 4.000000)");
+}
 
+TEST(AddTest, AddDec){
+     Base* four = new Op(4.123);
+     Base* five = new Op(5.523);
+     Base* test = new Add(four, five);
+     EXPECT_EQ(test->evaluate(), 9.646);
+     EXPECT_EQ(test->stringify(), "(4.123000 + 5.523000)");
+}
+ 
 
+TEST(AddTest, AddZero){
+    Base* zero = new Op(0);
+    Base* thirteen = new Op(13);
+    Base* test = new Add(zero, thirteen);
+    EXPECT_EQ(test->evaluate(), 13);
+    EXPECT_EQ(test->stringify(), "(0.000000 + 13.000000)");
+}
+
+TEST(AddTest, AddNeg){
+     Base* negTwo = new Op(-2);
+     Base* negEleven = new Op(-11);
+     Base* test = new Add(negTwo, negEleven);
+     EXPECT_EQ(test->evaluate(), -13);
+     EXPECT_EQ(test->stringify(), "(-2.000000 + -11.000000)");
+}
+
+TEST(AddTest, AddPosNeg){
+     Base* nine = new Op(9);
+     Base* negFifteen = new Op(-15);
+     Base* test = new Add(nine, negFifteen);
+     EXPECT_EQ(test->evaluate(), -6);
+     EXPECT_EQ(test->stringify(), "(9.000000 + -15.000000)");
+}
+
+TEST(AddTest, AddNegPos){
+     Base* neg_one = new Op(-1);
+     Base* five = new Op(5);
+     Base* test = new Add(neg_one, five);
+     EXPECT_EQ(test->evaluate(), 4);
+     EXPECT_EQ(test-> stringify(), "(-1.000000 + 5.000000)");
+}
+
+TEST(AddTest, DecAddNegPos){
+    Base* first = new Op(-2.5);
+    Base* second = new Op(5.0);
+    Base* test = new Add(first, second);
+    EXPECT_EQ(test->evaluate(), 2.5);
+    EXPECT_EQ(test->stringify(), "(-2.500000 + 5.000000)");
+}
+
+TEST(AddTest, DecAddPosPos){
+    Base* first = new Op(8.0);
+    Base* second = new Op(11.8);
+    Base* test = new Add(first, second);
+    EXPECT_EQ(test->evaluate(), 19.8);
+    EXPECT_EQ(test->stringify(), "(8.000000 + 11.800000)");
+}
 #endif //__OP_TEST_HPP__
